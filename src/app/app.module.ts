b7197366b0ed6c7a9b44b7b64318431f2a115309
Everett1914 <qwest1914@gmail.com>
2021-09-23 22:13:45 -0400

    Added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
