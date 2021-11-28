a6c4d03777645a2dc46f79982d13b32831bb3752
Everett1914 <qwest1914@gmail.com>
2021-11-28 09:10:52 -0500

    Added a filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -20,8 +24,11 @@ import { MoviesComponent } from './movies/movies.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     MatTableModule,
     MatButtonToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
     HttpClientModule,
     FormsModule,
   ],
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { ShowElement } from '../ShowElement';
 import { ShowsService } from '../shows.service';
 @Component({
@@ -14,6 +15,7 @@ export class MoviesComponent implements OnInit {
   constructor( private showsService: ShowsService ) { }
 
   displayedColumns: string[] = ['position', 'name', 'genre', 'source', 'cost', 'type'];
+  dataSource = new MatTableDataSource<ShowElement>();
 
   ngOnInit(): void {
     this.getAllShows();
@@ -21,7 +23,11 @@ export class MoviesComponent implements OnInit {
 
   getAllShows(): void {
     this.showsService.getAllShows()
-    .subscribe(shows => this.shows = shows);
+    .subscribe(shows => this.dataSource = new MatTableDataSource(shows));
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
